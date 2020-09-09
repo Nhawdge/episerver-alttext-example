a123056dd519e9a76a044bb57963676bc36fef4e
John Pavek <john.pavek@blendinteractive.com>
2020-09-09 09:49:35 -0500

    Adds Alt Text Descriptor to try suggestion

---
+++ Alloy/Business/EditorDescriptors/AltTextDescriptor.cs
@@ -0,0 +1,15 @@
+﻿using Alloy.Models.Media;
+using EPiServer.Shell;
+
+namespace Alloy.Business.EditorDescriptors
+{
+    public class AltTextDescriptor : UIDescriptor<ImageFile>, IEditorDropBehavior
+    {
+        public EditorDropBehavior EditorDropBehaviour { get; set; }
+
+        public AltTextDescriptor() : base()
+        {
+            EditorDropBehaviour = EditorDropBehavior.CreateContentBlock;
+        }
+    }
+}
